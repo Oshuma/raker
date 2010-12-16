2b4ad43af1481911dd7ec6187bf6ba61c31e8b6c
Felipe Oliveira <felipe.oliveira@abril.com.br>
2010-12-16 15:25:08 -0200

    figure out where is rake command. do not assume that it is in PATH

--- lib/rack/raker/task_manager.rb
+++ lib/rack/raker/task_manager.rb
@@ -11,9 +11,13 @@ module Rack
         @rakefile = rakefile
       end
 
+      def rake
+        @rake ||= %x[ whereis rake ].match(/rake: ([^\s]+)/)[1]
+      end
+
       # Return an array of Rake tasks.
       def tasks
-        output = %x[ rake -f #{@rakefile} -s -T ]
+        output = %x[ #{rake} -f #{@rakefile} -s -T ]
         @tasks ||= output.split("\n").map do |task|
           task.match(/^rake\s([^\s]+)\s+#\s(.+)/)[1..2]
         end.compact
@@ -22,7 +26,7 @@ module Rack
       # Run the specified Rake +task+ and returns the (string) output, or false upon failure.
       # TODO: Maybe move the +rake_opts+ elsewhere.
       def run(task, rake_opts = ['-s'])
-        output = %x[ rake -f #{@rakefile} #{rake_opts.join(' ')} #{task} 2>/dev/null ]
+        output = %x[ #{rake} -f #{@rakefile} #{rake_opts.join(' ')} #{task} 2>/dev/null ]
         output = false if output.empty?
         output
       end
